16c08e629c2c618f3756cbb22d0527d6434c9efd
Nathan Blanchard <nathanblanchard@Nathans-MacBook-Pro-3.local>
2020-06-08 21:14:06 -0400

    test

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 9090,
   NODE_ENV: process.env.NODE_ENV || "development",
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://ngblanchard:NaZsIstAtVWuNeam@alcovereads-cflny.mongodb.net/AlcoveReads?retryWrites=true&w=majority",
+  MONGODB_URL: process.env.MONGODB_URL,
   MONGO_DB_NAME: process.env.MONGO_DB_NAME || "AlcoveReads",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
 };
